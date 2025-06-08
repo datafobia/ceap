4ec755b1b8f27356980a164e8a90a266197cc9b3
datafobia <dalson.figueiredofo@ufpe.br>
2025-06-08 07:24:07 -0300

    Initial commit

---
+++ R/ceap_data.R
@@ -0,0 +1,8 @@
+#' Base de Dados CEAP
+#'
+#' Esta base contém dados do Centro de Estudos e Aperfeiçoamento da Administração Pública (CEAP).
+#'
+#' @format Um data frame com N linhas e M colunas. *(Edite depois com `str(ceap_data)` para ajustar)*
+#' @usage data(ceap_data)
+#' @source Dados internos fornecidos para uso técnico.
+"ceap_data"
